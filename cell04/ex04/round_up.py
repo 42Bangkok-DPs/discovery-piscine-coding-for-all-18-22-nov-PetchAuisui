de2279397f8ad36337710e063e1445bf846b3c25
PetchAuisui <petchauisui50Ggmail.com>
2024-11-20 16:31:51 +0700

    13

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python1
+number = float(input("Give me a number: "))
+if number == int(number):
+  rounded_number = int(number)
+else:
+  rounded_number = int(number) + 1
+print(rounded_number)
