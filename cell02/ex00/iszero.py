d35ac14d5031be57f901714e4c038424ed031e4f
PetchAuisui <petchauisui50Ggmail.com>
2024-11-20 09:33:23 +0700

    02

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -1,5 +1,9 @@
-number = int(input("Please enter a number: "))
-if number == 0:
-     print("This number is equal to zero.")
-else:
-     print("This number is different from zero.")
+#!/usr/bin/env python
+try:
+  number = int(input("Please enter a number: "))
+  if number == 0:
+    print("This number is equal to zero.")
+  else:
+    print("This number is different from zero.")
+except ValueError:
+  print("Invalid 1  input. Please enter a valid integer.")
