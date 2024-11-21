451007d1222b80feca9eed4a01ef6a5ff63864fc
PetchAuisui <petchauisui50Ggmail.com>
2024-11-21 21:51:39 +0700

    26

--- rush/main.py
+++ rush/main.py
@@ -1,11 +1,12 @@
 from checkmate import checkmate
+
 def main():
+    # ตัวอย่างที่ 1
     board = """\
-R...
-.K..
-..P.
-....\
-"""
-    checkmate(board)
+..
+.K\
+""" 
+    print(checkmate(board))  # Expected Output: Success
+
 if __name__ == "__main__":
     main()
