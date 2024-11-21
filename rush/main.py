9a072a5b715d22e89525e3849115a82443326947
PetchAuisui <petchauisui50Ggmail.com>
2024-11-21 16:20:16 +0700

    ed

--- rush/main.py
+++ rush/main.py
@@ -1,13 +1,10 @@
 from checkmate import checkmate
 def main():
-    # กำหนดกระดานหมากรุก (board) โดยตรง
     board = """\
 R...
 .K..
 ..P.
 ...."""
-    
-    # เรียกใช้ฟังก์ชัน checkmate เพื่อตรวจสอบกระดาน
     checkmate(board)
 
 if __name__ == "__main__":
